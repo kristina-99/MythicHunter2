2769ba87d15a63d2d9585aa180bd036b6075a11f
kristina-99 <62075853+kristina-99@users.noreply.github.com>
2026-02-10 15:50:53 +0200

    Add files via upload
    * added an Item class
    * added an Armor class

---
+++ MythicHunter2/Items/Armor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MythicHunter2.Items
+{
+    public class Armor : Item
+    {
+        public Armor(int positionY, int PositionX) : base(positionY, PositionX)
+        {
+
+        }
+
+        public override void ActivateSpecialEffect()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MythicHunter2/Items/Item.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MythicHunter2.Items
+{
+    public abstract class Item
+    {
+        private string name;
+        private int positionY;
+        private int positionX;
+
+        public Item(int positionY, int PositionX)
+        { 
+            this.PositionY = positionY;
+            this.PositionX = PositionX;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+        
+        public int PositionY
+        {
+            get 
+            {
+                return this.positionY;
+            }
+            set
+            {
+                this.positionY = value;
+            }
+        }
+
+        public int PositionX
+        {
+            get
+            {
+                return this.positionX;
+            }
+            set
+            { 
+                this.PositionX = value;
+            }
+        }
+
+        public abstract void ActivateSpecialEffect();
+    }
+}
