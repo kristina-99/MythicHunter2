00c12dbe2f2a936c3c1ef1ae569c49454a79cb64
kristina-99 <kristina_atan@abv.bg>
2026-02-11 21:38:48 +0200

    * created a Magic class

---
+++ MythicHunter2/Items/Magic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MythicHunter2.Items
+{
+    public class Magic : Item
+    {
+        public Magic(int positionY, int PositionX) : base(positionY, PositionX)
+        {
+
+        }
+
+        public override void ActivateSpecialEffect()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
