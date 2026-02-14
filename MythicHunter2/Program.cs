d1de6868ebf46f91996dfa51902a8c89a496be19
kristina-99 <kristina_atan@abv.bg>
2026-02-14 16:39:57 +0200

    * added a GenerateMap method to the Program class

--- MythicHunter2/Program.cs
+++ MythicHunter2/Program.cs
@@ -10,6 +10,45 @@ namespace MythicHunter2
     {
         static void Main(string[] args)
         {
+            bool gameOver = false;
+
+
+        }
+
+        static char[,] GenerateMap(int n, int m)
+        {
+            char[,] map = new char[n, m];
+
+            Random rng = new Random();
+
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    map[i, j] = '.';
+                }
+            }
+
+            for (int i = 0; i <= 2; i++)
+            {
+                int y = rng.Next(0, n - 1);
+                int x = rng.Next(0, m - 1);
+                if (map[y, x] == '.')
+                {
+                    map[y, x] = '#';
+                }
+                else
+                {
+                    while (map[y, x] != '.')
+                    {
+                        y = rng.Next(0, n - 1);
+                        x = rng.Next(0, m - 1);
+                    }
+                    map[y, x] = '#';
+                }
+            }
+
+            return map;
         }
     }
 }
