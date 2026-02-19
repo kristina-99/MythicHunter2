3e493c8bd3ad149b5406ec114c0489d366b6edf1
kristina-99 <kristina_atan@abv.bg>
2026-02-19 15:44:48 +0200

    * created Enum Items

---
+++ MythicHunter2/Items/Enumerations/Items.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MythicHunter2.Items.Enumerations
+{
+    public enum Items
+    {
+        Armor,
+        Magic,
+        Potion,
+        Weapon
+    }
+}
